9f06b911be1bb5082d01aeeadb2bd44400de91e2
Matthew J Brown <matthew@lokislayer.com>
2023-11-15 15:33:35 -0600

    Loads the DbConfig for use plus fetches the connection string.

---
+++ Flashcards.Barakisbrown/Flashcards.Barakisbrown/Data/Configuration.cs
@@ -0,0 +1,36 @@
+﻿namespace Flashcards.Barakisbrown.Data;
+
+using Flashcards.Barakisbrown.Model;
+using Microsoft.Extensions.Configuration;
+
+
+public static class Configuration
+{
+    private static string ConnStringName = "MyDB";
+
+    public static DbConfig LoadSettings()
+    {
+        var path = AppDomain.CurrentDomain.BaseDirectory + "\\Properties";
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(path)
+            .AddJsonFile("appsettings.json", false);
+
+        IConfiguration configuration = builder.Build();
+
+        return configuration.GetSection("AppKeys").Get<DbConfig>();
+    }
+
+    public static string GetConnectionString()
+    {
+        var path = AppDomain.CurrentDomain.BaseDirectory + "\\Properties";
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(path)
+            .AddJsonFile("appsettings.json", false);
+
+        IConfiguration configuration = builder.Build();
+
+        return configuration.GetConnectionString(ConnStringName);
+    }
+}
