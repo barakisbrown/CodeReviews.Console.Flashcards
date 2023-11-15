bfe3c8113fdd28651e97a7774d9136cef0e29492
Matthew J Brown <matthew@lokislayer.com>
2023-11-15 15:33:12 -0600

    POCO for my app settings for DB Configuration, etc.

---
+++ Flashcards.Barakisbrown/Flashcards.Barakisbrown/Model/DbConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace Flashcards.Barakisbrown.Model;
+
+public class DbConfig
+{
+    public string InitDbScript { get; set; } = string.Empty;
+    public string InitCardTableScript { get; set; } = string.Empty;
+    public string InitStackTableScript { get; set;} = string.Empty;
+    public string NotImplementedMsg { get; set; } = string.Empty;
+    public string DbName { get; set; } = string.Empty;
+    public string CardTableName { get; set; } = string.Empty;
+    public string StackTableName { get; set; } = string.Empty;
+}
