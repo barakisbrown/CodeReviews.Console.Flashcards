e5dbd28278b751fba35056f393c6dc2602785f8e
Matthew J Brown <matthew@lokislayer.com>
2023-11-15 16:26:19 -0600

    Card POCO

---
+++ Flashcards.Barakisbrown/Flashcards.Barakisbrown/Model/Card.cs
@@ -0,0 +1,10 @@
+﻿namespace Flashcards.Barakisbrown.Model;
+
+public class Card
+{
+    public int CardID { get; set; }
+    public string? Prompt { get; set; }
+    public string? Answer { get; set; }
+    public int? StackID { get; set; }
+
+}
