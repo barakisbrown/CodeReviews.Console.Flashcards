e9ff7092d70a6001d7572e7903bf1aa51655817e
Matthew J Brown <matthew@lokislayer.com>
2023-11-15 15:34:17 -0600

    Init Commit

---
+++ Flashcards.Barakisbrown/Flashcards.Barakisbrown/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using Flashcards.Barakisbrown.Data;
+
+var config = Configuration.LoadSettings();
+
+Console.WriteLine($"The SQL for InitDB => {config.InitDbScript}");
+Console.WriteLine($"Connection String => {Configuration.GetConnectionString()}");
