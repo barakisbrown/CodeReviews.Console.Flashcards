f222d6f9b3b58defa8dc2c632e029cb6912e5409
barakis <lokislayer@gmail.com>
2026-08-11 01:03:08 -0500

    UI Stuff

---
+++ Flashcards.Barakisbrown/UI/MainMenu.cs
@@ -0,0 +1,65 @@
+﻿using DataLayer.Controller;
+using DataLayer.Models;
+using Microsoft.SqlServer.Management.XEvent;
+using Spectre.Console;
+
+namespace UI
+{
+    public class MainMenu : IMenu
+    {
+        private readonly string _appName = "Flashcards App";
+        private readonly string _appNote = "Table DEFAULT can not be deleted since it is a system table.";
+        private readonly StackController _stack;
+        private readonly CardController _card;
+        private string[] _menu = [];
+
+        public MainMenu(CardController card, StackController stack)
+        {
+            _card = card;
+            _stack = stack;
+            LoadMenu(IMenu.MainMenuTitle);
+
+        }
+
+        public CardController CardCtr { get => _card; init => _card = value; }
+        public StackController StackCtr { get => _stack; init  => _stack = value; }
+        public void DisplayMenu()
+        {
+            while (true)
+            {
+                AnsiConsole.Clear();
+                AnsiConsole.Write(new FigletText(_appName).Color(Color.Green));
+                AnsiConsole.WriteLine();
+
+                var choice = AnsiConsole.Prompt(new SelectionPrompt<string>()
+                        .Title("Select an option:").AddChoices(_menu));
+
+                 switch (choice)
+                {
+                    case "Study Sessions":
+                        var session = new StudySession.Sessions(_card, _stack);
+                        session.DisplayMenu();
+                        continue;
+                    case "FlashCards":
+                        new CardMenu(_card,_stack).DisplayMenu();
+                        continue;
+                    case "Stacks":
+                        new StackMenu(_card,_stack).DisplayMenu();
+                        continue;
+                    case "Exit":
+                        break;
+                }
+
+                break;
+            }
+        }
+        public void LoadMenu(string menuName)
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            path += "//MENUS//";
+
+            _menu = File.ReadAllLines(Path.Combine(path, menuName));
+
+        }       
+    }
+}
---
+++ Flashcards.Barakisbrown/UI/MenuHelper.cs
@@ -0,0 +1,57 @@
+﻿
+using DataLayer.Controller;
+using DataLayer.Models;
+using Microsoft.SqlServer.Management.HadrModel;
+using Spectre.Console;
+
+namespace UI;
+
+public static class MenuHelper
+{
+    private readonly static CardController CardController = new();
+    private static StackController _stackController = new();
+    
+    public static Stack GetStackNamePrompt(List<Stack>? stackList)
+    {
+        const string title = $"  STACK";
+        var prompt = new SelectionPrompt<Stack>()
+            .Title(title)
+            .UseConverter(s => $"[bold]{s.Name}[/]")
+            .AddChoices<Stack>(stackList);
+        return AnsiConsole.Prompt(prompt);
+    }
+    
+    public static Card GetCardNamesPrompt(int stackID)
+    {
+        var list = CardController.GetAllCardsByStack(stackID);
+        list.Add(new DataLayer.Models.Card
+        {
+            Answer = "CANCEL",
+            Prompt = "EDIT"
+        });
+
+        var title = $"{"  Prompt",-15} {" Answer"}";
+        
+        var prompt = new SelectionPrompt<Card>()
+            .Title(title)
+            .UseConverter(s => $"{s.Prompt,-15} {s.Answer}")
+            .AddChoices<Card>(list);
+        return AnsiConsole.Prompt(prompt);
+    }
+
+    public static Stack GetStackListPrompt(List<Stack> stackList, string title)
+    {
+        var prompt = new SelectionPrompt<Stack>()
+            .Title(title)
+            .UseConverter(s => $"[bold]{s.Name}[/]")
+            .AddChoices<Stack>(stackList);
+        return AnsiConsole.Prompt(prompt);
+    }
+
+    public static string GetTextPrompt(string prompt)
+    {
+        var prmpt = new TextPrompt<string>(prompt)
+                .AllowEmpty();
+        return AnsiConsole.Prompt(prmpt);
+    }   
+}
