eeb49bc44f104afec452361e7b9f47cbfdaec472
barakis <lokislayer@gmail.com>
2026-08-11 00:56:19 -0500

    All DB Related Stuff

---
+++ Flashcards.Barakisbrown/DataLayer/Controllers/CardController.cs
@@ -0,0 +1,162 @@
+﻿using Dapper;
+using DataLayer.Models;
+using DataLayer.Models.DTO;
+using Microsoft.Data.SqlClient;
+using Spectre.Console;
+
+namespace DataLayer.Controller;
+
+public class CardController
+{
+    private readonly static DbUser DataSource = Configuration.GetUserSecretsConnStrings();
+    private const string SelectAllSql = "SELECT * FROM dbo.Cards";
+    private const string InsertSql = "INSERT INTO dbo.Cards(Prompt, Answer, StackID) VALUES(@PROMPT, @ANSWER, @STACKID)";
+    private const string UpdateSql = "Update dbo.cards SET Prompt = @Prompt, Answer = @Answer, StackID = @StackID" + " WHERE dbo.cards.ID = @ID";
+    private const string DeleteSql = "DELETE FROM dbo.Cards WHERE dbo.Cards.ID = @ID";
+    private const string CardsPerStackSql = "SELECT COUNT(*) FROM Cards WHERE Cards.StackID = @fkey";
+
+    public CardController()
+    {
+        
+    }
+
+    private SqlConnection MakeConnection
+    {
+        get
+        {
+            var conn = new SqlConnection(DataSource.Main);
+            if (conn.State != System.Data.ConnectionState.Open)
+            {
+                try
+                {
+                    conn.Open();
+                }
+                catch(SqlException e)
+                {
+                    AnsiConsole.MarkupLineInterpolated($"[blink red]Error problem opening connection to the database engine. CHeck to see if it running.[/]");
+                    throw;
+                }
+            }
+            return conn;
+        }
+    }
+
+    /*
+     * ADDCARD / EDIT CARD / GET CARD / UPDATE CARD / DELETE CARD
+     * 
+     * CREATE COMPLETED
+     * READ COMPLETED
+     * UPDATE COMPLETED
+     * DELETE COMPLETED
+     */
+    public (bool,string) AddCard(string Prompt, string Answer, int fkey = 1)
+    {
+        bool success;
+        string message = string.Empty;
+        // CHECK STRINGS FIRST
+        if (string.IsNullOrEmpty(Prompt) || string.IsNullOrEmpty(Answer))
+            return (false, "Prompt or Answer is either null or Empty!");
+        // CREATE CONNECTION
+        using var conn = MakeConnection;
+        object[] param = { new { Prompt, Answer, @STACKID = fkey } };        
+        try
+        {
+            var added = conn.Execute(InsertSql, param) == 1;
+            success = true;
+        }
+        catch(Exception e)
+        {
+            success = false;
+            message = "Not Added";
+        }
+
+        return (success, message);
+    }
+
+    public (bool,string) AddCard(Card newCard)
+    {
+        return AddCard(newCard.Prompt, newCard.Answer, newCard.StackID);
+
+    }
+
+    // READ SECTION BEGIN
+
+    public List<Card> GetAllCards()
+    {
+        using var conn = MakeConnection;
+        return conn.Query<Card>(SelectAllSql).ToList();
+    }
+
+    public int Count => GetAllCards().Count;
+
+    public Card GetCardByID(int ID) => GetAllCards().FirstOrDefault(x => x.ID == ID);
+
+    public List<Card> GetAllCardsByStack(int stackID) => GetAllCards().Where(x => x.StackID == stackID).ToList();
+
+    public List<CardDTO> DisplayCardsByStack(int stackID)
+    {
+        var list = new List<CardDTO>();
+        var cards = GetAllCards().Where(x => x.StackID == stackID).ToList();
+        foreach(var single in cards)
+        {
+            var one = new CardDTO
+            {
+                Front = single.Prompt,
+                Back = single.Answer
+            };
+            list.Add(one);
+        }
+
+        return list;
+    }
+
+    public int GetNumberCardsInStack(int fkey) => GetAllCardsByStack(fkey).Count;
+
+    public bool CardExist(Card selCard)
+    {
+        var list = GetAllCardsByStack(selCard.StackID);
+        var exist = list.Any(x => x.Prompt.Equals(selCard.Prompt) && (x.Answer.Equals(selCard.Answer)));
+        return exist;
+    }
+
+    // READ SECTION END
+    // UPDATE SECTION BEGIN
+    public (bool,string) EditCard(Card updatedCard)
+    {
+        bool success = false;
+        string message = string.Empty;
+        using var conn = MakeConnection;
+        object[] param = { new { updatedCard.Prompt, updatedCard.Answer, updatedCard.StackID, updatedCard.ID} };
+
+        try
+        {
+            var updated = conn.Execute(UpdateSql, param) == 1;
+            success = true;
+        }
+        catch (Exception e)
+        {
+            success = false;
+            message = e.Message;
+        }       
+        return (success, message); 
+    }
+    
+    // UPDATE SECTION END
+    // DELETE SECTION BEGIN
+    public (bool,string) DeleteCard(Card deletedCard)
+    {
+        bool success = false;
+        string message = string.Empty;
+        using var conn = MakeConnection;
+        object[] param = { new { deletedCard.ID } };
+        try {
+            var deleted = conn.Execute(DeleteSql, param) == 1;
+            success = deleted;
+        } catch (Exception e) 
+        {
+            success = false;
+            message = e.Message;
+        }
+        return (success, message);
+    }
+}
---
+++ Flashcards.Barakisbrown/DataLayer/Controllers/SessionController.cs
@@ -0,0 +1,130 @@
+﻿namespace DataLayer.Controller;
+
+using Dapper;
+using DataLayer.Models;
+using DataLayer.Models.DTO;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Reflection;
+
+public class SessionController
+{
+    private readonly StackController _stack = new();
+    private readonly string _tableName = "dbo.Sessions";
+    private readonly static DbUser _dataSource = Configuration.GetUserSecretsConnStrings();
+    private readonly string _selectSQL;
+    private readonly string _insertSQL;
+    private List<Session> _sessions = new();
+
+    public SessionController(StackController stack)
+    {
+        _selectSQL = "SELECT * FROM " + _tableName;
+        _insertSQL = "INSERT INTO " + _tableName + " (StudyDate,StudyScore,StackID,StackName,NumQuestions)";
+        _insertSQL += "values (@StudyDate,@StudyScore,@StackID,@StackName,@NumQuestions)";
+        _sessions = GetAllSessions();
+        _stack = stack;
+    }
+
+    /// <summary>
+    /// Selects all Session data and returns it
+    /// </summary>
+    /// <returns>List of Session Data</returns>
+    public List<Session> GetAllSessions()
+    {
+        using var conn = MakeConnection;
+        return conn.Query<Session>(_selectSQL).ToList();
+    }
+
+    /// <summary>
+    /// Propery that returns a SqlConnection
+    /// </summary>
+    private SqlConnection MakeConnection
+    {
+        get
+        {
+            var conn = new SqlConnection(_dataSource.Main);
+            if (conn.State != System.Data.ConnectionState.Open)
+            {
+                try { conn.Open(); } catch (SqlException) { throw; }                   
+            }
+            return conn;
+        }
+    }
+    /// <summary>
+    /// Add(Insert) a new Session Data[datetime and score]
+    /// </summary>
+    /// <param name="data">Datetime and score of a paticular session</param>
+    /// <returns>added,message => added is true then message is blank else message will be the exception message</returns>
+    public (bool,string) AddSession(Session data)
+    {
+        string message = string.Empty;
+        bool added = false;
+        if (string.IsNullOrEmpty(data.StackName)) added = false;
+        else
+        {
+            try
+            {                
+                using var conn = MakeConnection;
+                DynamicParameters param = new();
+                param.Add("StudyDate", data.StudyDate,direction: ParameterDirection.Input);
+                param.Add("StudyScore", data.StudyScore, direction: ParameterDirection.Input);
+                param.Add("StackID", data.StackID, direction: ParameterDirection.Input);
+                param.Add("StackName", data.StackName, direction: ParameterDirection.Input);
+                param.Add("NumQuestions", data.NumQuestions, direction: ParameterDirection.Input);
+                param.Add("RowsInserted", dbType: DbType.Int32, direction: ParameterDirection.Output);
+                conn.Execute("AddNewSession", param, commandType: CommandType.StoredProcedure);
+                int rows = param.Get<int>("RowsInserted");
+                if (rows == 1)
+                {
+                    added = true;
+                    _sessions = GetAllSessions();
+                }
+            }
+            catch (SqlException cmd)
+            {
+                added = false;
+                message = cmd.Message;
+            }
+        }
+        return (added,message);
+    }
+
+    public (bool,string) AddSession(SessionDTO Data,int StackID)
+    {
+        Session data = new()
+        {
+            StackName = Data.StackName,
+            StudyDate = Data.Completed,
+            NumQuestions = Data.TotalQuestions,
+            StudyScore = Data.Score,
+            StackID = StackID
+        };
+        return AddSession(data);
+    }
+
+    /// <summary>
+    /// Returns how many session have been logged in the system
+    /// </summary>
+    public int COUNT => _sessions.Count;
+    
+    public IReadOnlyList<SessionDTO> GetUserSessionData()
+    {
+        _sessions = GetAllSessions();
+        var dTOs  = new List<SessionDTO>();
+
+        foreach(var single in _sessions)
+        {
+            var one = new SessionDTO
+            {
+                StackName = single.StackName,
+                TotalQuestions = single.NumQuestions,
+                Completed = single.StudyDate,
+                Score = single.StudyScore
+            };
+            dTOs.Add(one);
+        }
+
+        return dTOs;
+    }
+
+}
