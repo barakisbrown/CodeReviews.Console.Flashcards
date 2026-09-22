47f909119120e7d6a5e88b90bd1f2b331bd17a60
barakis <lokislayer@gmail.com>
2026-09-22 18:26:02 -0500

    Static Class for use with Session Object

---
+++ Flashcards.Barakisbrown/StudySession/Helper.cs
@@ -0,0 +1,22 @@
+﻿using Spectre.Console;
+using DataLayer.Models;
+namespace StudySession;
+
+public static class Helper
+{
+    public static SelectionPrompt<Stack> GetStackNameList(List<Stack> stackList)
+    {
+        string title = Environment.NewLine + "Select Flash Card Stack to Study.";
+        var prompt = new SelectionPrompt<Stack>()
+            .Title(title)
+            .UseConverter(s => $"[bold]{s.Name}[/]")
+            .AddChoices<Stack>(stackList);
+        return prompt;
+    }
+
+    public static int GetRandomFlashCardFromList(int totalCards)
+    {
+        var rnd = new Random();
+        return rnd.Next(1, totalCards);
+    }
+}
